ed676737f8b98d7595caba77929fe17f93eb8ba1
ShahabSamariShams <shahabshams242@gmail.com>
2024-05-29 14:43:19 +0330

    Mark Class
    An initial model of the "Mark" entities.

---
+++ mark.h
@@ -0,0 +1,25 @@
+#include <string>
+
+#include "city.h"
+
+enum Color{
+    white,
+    black,
+    red,
+    blue,
+    green,
+    yellow,
+    purple,
+    lightBlue
+};
+
+class Mark{
+    public:
+        Mark();
+        City whereIsIt()const;
+        void setMarkOn(City*);
+        void setColor(Color);
+    private:
+        City* cityAddress;
+        Color color;
+};
