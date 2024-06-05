601466de46246d88541530033838f61d38d98c0c
ShahabSamariShams <shahabshams242@gmail.com>
2024-06-05 21:23:33 +0330

    Adding game initial class data members.

--- game.h
+++ game.h
@@ -1,4 +1,17 @@
+#include <vector>
+
+#include "card.h"
+#include "player.h"
+#include "mark.h"
+#include "map.h"
+
 class Game{
     public:
+        
     private:
+        std::vector <Card*> cards;
+        std::vector <Card*> burntCards;
+        std::vector <Player> playerList;
+        Mark blackMark;
+        Map theMap;
 };
