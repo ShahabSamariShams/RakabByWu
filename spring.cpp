d6db3fb2559bc4f363a5abe26957f087a075c4b4
ShahabSamariShams <shahabshams242@gmail.com>
2024-06-07 10:48:15 +0330

    Spring class initialized.

---
+++ spring.cpp
@@ -0,0 +1,10 @@
+#include "spring.h"
+
+Spring::Spring(){
+    setPriority(4);
+    setPower(0);
+}
+
+void ability(Game currentGame){
+    
+}
---
+++ spring.h
@@ -0,0 +1,13 @@
+#ifndef SPRING_H
+
+#define SPRING_H
+#include "purpleCard.h"
+
+class Spring: public PurpleCard{
+    public:
+        Spring();
+        void ability(Game);
+    private:
+};
+
+#endif
