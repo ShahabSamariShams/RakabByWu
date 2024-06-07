df3b7ae11bc6bdee7cc84e1c42f25a71c793aaab
ShahabSamariShams <shahabshams242@gmail.com>
2024-06-07 23:45:49 +0330

    Initial ability function for the spring.

--- spring.cpp
+++ spring.cpp
@@ -7,5 +7,21 @@ Spring::Spring(){
 
 void ability(Game& currentGame){
     int highestCard = currentGame.highestYellowCardPlayed();
-    //for(auto i: )
+    std::vector <Player> tempPlayerList = currentGame.getPlayerList();
+    std::vector <Card*> tempYellowArmy;
+    int playerTempHighest;
+    for(int i = 0; i < tempPlayerList.size(); i++){
+        playerTempHighest = tempPlayerList[i].highestYellowCard();
+        if(highestCard == playerTempHighest){
+            tempYellowArmy = tempPlayerList[i].getYellowArmy();
+            int counter = 0;
+            for(int i = 0; i < tempYellowArmy.size(); i++){
+                if(tempYellowArmy[i]->getPower() == highestCard){
+                    counter++;
+                }
+            }
+            tempPlayerList[i].addToArmyPower(counter * 3);
+        }
+    }
+    currentGame.setPlayerList(tempPlayerList);
 }
