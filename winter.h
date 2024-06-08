28b75c4a3cb6ab0ccce2482c4876bdac587cad52
ShahabSamariShams <shahabshams242@gmail.com>
2024-06-08 23:15:12 +0330

    Create winter.h

---
+++ winter.h
@@ -0,0 +1,13 @@
+#ifndef WINTER_H
+
+#define WINTER_H
+#include "purpleCard.h"
+
+class Winter: public PurpleCard{
+    public:
+        Winter();
+        void ability(Game&);
+    private:
+};
+
+#endif
