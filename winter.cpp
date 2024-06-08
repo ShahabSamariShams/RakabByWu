b532eeef43ea5eb9b060842d56187c21391fec4f
ShahabSamariShams <shahabshams242@gmail.com>
2024-06-08 23:22:20 +0330

    Created winter.cpp.
    Wrote it's constructor and over-rided ability function.

---
+++ winter.cpp
@@ -0,0 +1,13 @@
+#include "winter.h"
+
+Winter::Winter(){
+    setPriority(2);
+    setPower(0);
+}
+
+void Winter::ability(Game& currentGame){
+    std::vector <Player> tempPlayerList = currentGame.getPlayerList();;
+    for(int i = 0; i < tempPlayerList.size(); i++){
+        tempPlayerList[i].setArmyPower(tempPlayerList[i].yellowArmySize());
+    }
+}
