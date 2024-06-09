b1bfcbf3c462b595bbea6c00ec31905db4f34061
ShahabSamariShams <shahabshams242@gmail.com>
2024-06-09 22:25:39 +0330

    Drummer ability function.

--- drummer.cpp
+++ drummer.cpp
@@ -6,7 +6,17 @@ Drummer::Drummer(){
 }
 
 void Drummer::ability(Game& currentGame){
-    
+    Player* tempPlayerAddress = currentGame.playerInTurn();
+    if(currentGame.getSeason() == "winter"){
+        if(tempPlayerAddress->getArmyPower() != tempPlayerAddress->yellowArmySize() * 2){
+            tempPlayerAddress->setArmyPower(tempPlayerAddress->getArmyPower() * 2);
+        }
+    }
+    else{
+        if(tempPlayerAddress->getArmyPower() != tempPlayerAddress->getRawYellowPower() * 2){
+            tempPlayerAddress->setArmyPower(tempPlayerAddress->getArmyPower() * 2);
+        }
+    }
 }
 
 std::string Drummer::getTypeName()const{
