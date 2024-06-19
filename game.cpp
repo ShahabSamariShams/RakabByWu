dad509fb8da0e7fb81ce3dc4a8d04e1ec998f8d6
ShahabSamariShams <shahabshams242@gmail.com>
2024-06-19 21:52:36 +0330

    Vastly organized.
    Some parts of some functions are divided into more detailed but smaller functions.

--- game.cpp
+++ game.cpp
@@ -12,6 +12,48 @@
 
 #include "userinterface.h"
 
+//------------------------------------------------------------------
+
+void merge(std::vector <std::pair <Card*, Player*>>& playedPurpleCards, int start, int end, int middle){
+    int leftPointer = start, rightPointer = middle + 1;
+    std::vector <std::pair <Card*, Player*>> tempSorted;
+    while(leftPointer <= middle && rightPointer <= end){
+        if(playedPurpleCards[leftPointer].first->getPriority() > playedPurpleCards[rightPointer].first->getPriority()){
+            tempSorted.push_back(playedPurpleCards[leftPointer]);
+            leftPointer++;
+        }
+        else{
+            tempSorted.push_back(playedPurpleCards[rightPointer]);
+            rightPointer++;
+        }
+    }
+
+    while(leftPointer <= middle){
+        tempSorted.push_back(playedPurpleCards[leftPointer]);
+        leftPointer++;
+    }
+    while(rightPointer <= end){
+        tempSorted.push_back(playedPurpleCards[rightPointer]);
+        rightPointer++;
+    }
+
+    for(int i = start, j = 0; i <= end; i++, j++){
+        playedPurpleCards[i] = tempSorted[j];
+    }
+}
+
+void mergeSort(std::vector <std::pair <Card*, Player*>>& playedPurpleCards, int start, int end){
+    if(start >= end){
+        return;
+    }
+    int middle = (start + end) / 2;
+    mergeSort(playedPurpleCards, start, middle);
+    mergeSort(playedPurpleCards, middle + 1, end);
+    merge(playedPurpleCards, start, end, middle);
+}
+
+//------------------------------------------------------------------
+
 Game::Game(){
     //Inputs from the user.
     short numberOfPlayers = UserInterface::receiveNumberOfPlayers();
@@ -66,7 +108,33 @@ Game::Game(){
         pointerToCard = new YellowCard(10);
         deckOfCards.push_back(pointerToCard);
     }
+    //--------------------------------------
+    season = NULL;
 }
+
+//------------------------------------------------------------------
+
+void Game::setSeason(Card* seasonToBeSetted){
+    if(season != NULL)
+        deckOfCards.push_back(season);
+    season = seasonToBeSetted;
+}
+
+void Game::setPlayerList(std::vector <Player> inputList){
+    playerList = inputList;
+}
+
+//------------------------------------------------------------------
+
+std::vector <Player> Game::getPlayerList()const{
+    return playerList;
+}
+
+std::string Game::getSeason()const{
+    return season->getTypeName();
+}
+
+//------------------------------------------------------------------
 
 int Game::highestYellowCardPlayed(){
     int highestPower = playerList[0].highestYellowCard();
@@ -80,24 +148,24 @@ int Game::highestYellowCardPlayed(){
     return highestPower;
 }
 
-std::vector <Player> Game::getPlayerList()const{
-    return playerList;
+void Game::burnHandIfPossible(){
+    for(int i = 0; i < playerList.size(); i++){
+        if(!playerList[i].yellowCardInHand() && !playerList[i].emptyHand()){
+            if(UserInterface::receicveAnswer(playerList[i].getName()) == "yes"){
+                burnCards(playerList[i].burnCardsInHand());
+            }
+        }
+    }
 }
 
-void Game::setPlayerList(std::vector <Player> inputList){
-    playerList = inputList;
-}
-
-std::string Game::getSeason()const{
-    return season->getTypeName();
-}
-
-Player* Game::playerInTurn()const{
-    return playedPurpleCards.back().second;
+void Game::burnCards(std::vector <Card*> cardsToBurn){
+    for(int i = 0; i < cardsToBurn.size(); i++){
+        deckOfCards.push_back(cardsToBurn[i]);
+    }
 }
 
 void Game::distributeCards(){
-    std::shuffle(deckOfCards.begin(), deckOfCards.end(), std::default_random_engine(0));
+    std::shuffle(deckOfCards.begin(), deckOfCards.end(), std::default_random_engine(time(0)));
     std::vector <Card*> handtoBeGiven;
     for(int i = 0; i < playerList.size(); i++){
         handtoBeGiven.clear();
@@ -109,12 +177,84 @@ void Game::distributeCards(){
     }
 }
 
+bool Game::timeToDistribute(){
+    int indexOfTheSingleFullHand;
+    int count = 0;
+    for(int i = 0; i < playerList.size(); i++){
+        if(!playerList[i].emptyHand()){
+            count++;
+            indexOfTheSingleFullHand = i;
+            if(count >= 2){
+                return false;
+            }
+        }
+    }
+    if(count == 1){
+        burnCards(playerList[indexOfTheSingleFullHand].burnCardsInHand());
+    }
+    return true;
+}
+
 void Game::addToPlayedPurpleCards(Card* cardToBeAdded, Player* currentPlayer){
     if(cardToBeAdded != NULL){
         std::pair <Card*, Player*> toBeAdded{cardToBeAdded, currentPlayer};
         playedPurpleCards.push_back(toBeAdded);
     }
 }
+
+void Game::resetingArmies(){
+    while(playedPurpleCards.size() > 0){
+        deckOfCards.push_back(playedPurpleCards.back().first);
+        playedPurpleCards.pop_back();
+    }
+    for(int i = 0; i < playerList.size(); i++){
+        burnCards(playerList[i].burnYellowArmy());
+    }
+}
+
+void Game::calculateThePowers(){
+    for(int i = 0; i < playerList.size(); i++){
+        playerList[i].setArmyPower(playerList[i].getRawYellowPower());
+    }
+    std::vector <std::pair <Card*, Player*>>::iterator pointer; 
+    while(playedPurpleCards.size() > 0){
+        playedPurpleCards.back().first->ability(*this);
+        pointer = playedPurpleCards.end() - 1;
+        playedPurpleCards.erase(pointer);
+    }
+}
+
+//------------------------------------------------------------------
+
+void Game::setTheBlackMark(int indexOfWarStarter){
+    std::string cityName = UserInterface::callTheBlackMarkOwner(playerList[indexOfWarStarter], theMap);
+    blackMark.setMarkOn(theMap.toBeFoughtFor(cityName));
+}
+
+int Game::findTheYoungest(){
+    std::vector <int> indexOfTheYoungest;
+    int leastAge = playerList[0].getAge();
+    for(int i = 0; i < playerList.size(); i++){
+        if(playerList[i].getAge() < leastAge){
+            leastAge = playerList[i].getAge();
+            indexOfTheYoungest.clear();
+            indexOfTheYoungest.push_back(i);
+        }
+        else if(playerList[i].getAge() == leastAge){
+            indexOfTheYoungest.push_back(i);
+        }
+    }
+    int index = rand() % indexOfTheYoungest.size();
+    return indexOfTheYoungest[index];
+}
+
+//------------------------------------------------------------------
+
+Player* Game::playerInTurn()const{
+    return playedPurpleCards.back().second;
+}
+
+//------------------------------------------------------------------
 
 void Game::war(int& index){
     std::vector <bool> passed(playerList.size(), false);
@@ -123,12 +263,24 @@ void Game::war(int& index){
         if(!passed[i]){
             UserInterface::bringThePlayer(playerList[i].getName());
             while(true){
+                system("cls");
                 UserInterface::displayPlayersAndTheirPlayed(playerList, playedPurpleCards);
                 UserInterface::displayPlayersCities(playerList);
+                UserInterface::displayBlackMarkCity(blackMark.whereIsIt());
+                UserInterface::displaySeason(season);
                 playerInput = UserInterface::play(playerList[i]);
                 if(playerInput != "help" && playerInput != "empty"){
                     if(playerInput != "pass" && playerList[i].cardExistance(playerInput)){
-                        addToPlayedPurpleCards(playerList[i].playACard(playerInput), &playerList[i]); 
+                        addToPlayedPurpleCards(playerList[i].playACard(playerInput), &playerList[i]);
+                        if(playerInput == "scarecrow"){
+                            playedPurpleCards.back().first->ability(*this);
+                            deckOfCards.push_back(playedPurpleCards.back().first);
+                            playedPurpleCards.pop_back();
+                        }
+                        else if(playerInput == "winter" || playerInput == "spring"){
+                            setSeason(playedPurpleCards.back().first);
+                            playedPurpleCards.pop_back();
+                        }
                         break;
                     }
                     else if(playerInput == "pass"){
@@ -142,11 +294,13 @@ void Game::war(int& index){
                 }
             }
         }
-        if(i == playerList.size()){
-            i == 0;
+        if(i == playerList.size() - 1){
+            i = -1;
         }
     }
 }
+
+
 
 bool Game::endOfWar(std::vector <bool> passed){
     std::vector <int> indexOfNonPassers;
@@ -166,22 +320,7 @@ bool Game::endOfWar(std::vector <bool> passed){
     return true;
 }
 
-int Game::findTheYoungest(){
-    std::vector <int> indexOfTheYoungest;
-    int leastAge = playerList[0].getAge();
-    for(int i = 0; i < playerList.size(); i++){
-        if(playerList[i].getAge() < leastAge){
-            leastAge = playerList[i].getAge();
-            indexOfTheYoungest.clear();
-            indexOfTheYoungest.push_back(i);
-        }
-        else if(playerList[i].getAge() == leastAge){
-            indexOfTheYoungest.push_back(i);
-        }
-    }
-    int index = rand() % indexOfTheYoungest.size();
-    return indexOfTheYoungest[index];
-}
+
 
 Player* Game::whoWonTheWar(){
     std::vector <Player*> highestPowers;
@@ -206,44 +345,22 @@ Player* Game::whoWonTheWar(){
 }
 
 void Game::warWinnerAward(Player* winner){
-    if(winner != NULL){
-        std::vector <Mark> tempMarks = winner->getMarks();
-        for(int i = 0; i < tempMarks.size(); i++){
-            if(tempMarks[i].whereIsIt() == NULL){
-                tempMarks[i].setMarkOn(blackMark.whereIsIt());
-                winner->setMarks(tempMarks);
-                break;
-            }
+    std::vector <Mark> tempMarks = winner->getMarks();
+    for(int i = 0; i < tempMarks.size(); i++){
+        if(tempMarks[i].whereIsIt() == NULL){
+            tempMarks[i].setMarkOn(blackMark.whereIsIt());
+            winner->setMarks(tempMarks);
+            break;
         }
     }
 }
+
+//------------------------------------------------------------------
 
 bool Game::oneVicinity(std::vector <City*> adjacency){
     for(int i = 0; i < adjacency.size() - 1; i++){
-        for(int j = i; j < adjacency.size(); j++){
+        for(int j = i; j < adjacency.size(); j++){ 
             if(adjacency[i]->isAdjacent(adjacency[j]->getName())){
-                return true;
-            }
-        }
-    }
-}
-
-bool Game::gameWinner(Player* warWinner){
-    if(warWinner->numberOfTakenCities() == 5){
-        return true;
-    }
-
-    std::vector <Mark> marks = warWinner->getMarks();
-    std::vector <City*> adjacency; 
-    for(int i = 0; i < marks.size(); i++){
-        adjacency.resize(0);
-        if(marks[i].whereIsIt() != NULL){
-            for(int j = 0; j < marks.size(); j++){
-                if(marks[i].whereIsIt()->isAdjacent(marks[j].whereIsIt()->getName())){
-                    adjacency.push_back(marks[j].whereIsIt());
-                }
-            }
-            if(oneVicinity(adjacency)){
                 return true;
             }
         }
@@ -251,27 +368,68 @@ bool Game::gameWinner(Player* warWinner){
     return false;
 }
 
+//------------------------------------------------------------------
+
+bool Game::gameWinner(Player* warWinner){
+    if(warWinner->numberOfTakenCities() == 5){
+        return true;
+    }
+    if(warWinner->numberOfTakenCities() >= 3){
+        std::vector <Mark> marks = warWinner->getMarks();
+        std::vector <City*> adjacency; 
+        for(int i = 0; i < warWinner->numberOfTakenCities(); i++){
+            adjacency.clear();
+            for(int j = 0; j < warWinner->numberOfTakenCities(); j++){
+                if(marks[i].whereIsIt()->isAdjacent(marks[j].whereIsIt()->getName())){ 
+                    adjacency.push_back(marks[j].whereIsIt());
+                }
+            }
+            if(oneVicinity(adjacency)){
+                return true;
+            }  
+        }
+    }
+    return false;
+}
+
+
+
 void Game::runGame(){
     int indexOfWarStarter = findTheYoungest();
     while(true){
+        setTheBlackMark(indexOfWarStarter);
+        burnHandIfPossible();
+        if(timeToDistribute()){
+            distributeCards();
+        }
         war(indexOfWarStarter);
+
+        if(season != NULL){
+            std::pair <Card*, Player*> toBeAdded;
+            toBeAdded.first = season;
+            toBeAdded.second = NULL;
+            playedPurpleCards.push_back(toBeAdded);
+        }
+        mergeSort(playedPurpleCards, 0, playedPurpleCards.size() - 1);
+        std::reverse(playedPurpleCards.begin(), playedPurpleCards.end());
+        calculateThePowers();
+
         Player* winner = whoWonTheWar();
-        warWinnerAward(winner);
+        resetingArmies();
         if(winner != NULL){
+            warWinnerAward(winner);
+            UserInterface::announceTheLocalWarWinner(winner);
+            if(gameWinner(winner)){
+                UserInterface::announceTheWinner(winner);
+                return;
+            }
             for(int i = 0; i < playerList.size(); i++){
-                if(playerList[i].getName() == (*winner).getName()){
+                if(playerList[i].getName() == winner->getName()){
                     indexOfWarStarter = i;
                     break;
                 }
             }
+            blackMark.whereIsIt()->setFightability(false);
         }
-        gameWinner(winner);
     }
-}
-
-int main(){
-    //srand(time(0));
-    Game theGame;
-    theGame.runGame();
-    return 0;
 }
--- game.h
+++ game.h
@@ -14,35 +14,48 @@ class Player;
 
 class Game{
     public:
+    //Constructor and Destructor:
         Game();
-        int highestYellowCardPlayed();
-        std::vector <Player> getPlayerList()const;
+    //Setters:
+        void setSeason(Card*);
         void setPlayerList(std::vector <Player>);
+    //Getters:
+        std::vector <Player> getPlayerList()const;
         std::string getSeason()const;
-        Player* playerInTurn()const; //Player in turn for their played purpleCard.
+    //Cards related:
+        int highestYellowCardPlayed();
+        void burnHandIfPossible();
+        void burnCards(std::vector <Card*>);
         void distributeCards();
+        bool timeToDistribute();
         void addToPlayedPurpleCards(Card*, Player*);
-
-        bool oneVicinity(std::vector <City*>);
-
+        void resetingArmies();
+        void calculateThePowers();
+    //Black mark related:
+        int findTheYoungest();
+        void setTheBlackMark(int);
+    //Player related:
+        Player* playerInTurn()const; //Player in turn for their played purpleCard.
+    //War related:
         void war(int&);
         bool endOfWar(std::vector <bool>);
         Player* whoWonTheWar();
         void warWinnerAward(Player*);
+    //Helping functions:
+        bool oneVicinity(std::vector <City*>);
+    //Whole game related:
         bool gameWinner(Player*);
-
-        int findTheYoungest();
-
         void runGame();
 
-        private:
-        std::vector <Card*> deckOfCards;
-        std::vector <Card*> burntCards;
-        std::vector <std::pair<Card*, Player*> > playedPurpleCards;
+    private:
         std::vector <Player> playerList;
+
+        std::vector <Card*> deckOfCards;
+        std::vector <std::pair<Card*, Player*> > playedPurpleCards;
+        Card* season;
+
         Mark blackMark;
         Map theMap;
-        Card* season;
 
 };
 
