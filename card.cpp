7a10b5269417e43395d3c8e2efcc6e259ff1a622
ShahabSamariShams <shahabshams242@gmail.com>
2024-07-03 01:49:54 +0330

    Update card.cpp
    Removing purple card functions.

--- card.cpp
+++ card.cpp
@@ -13,19 +13,10 @@ void Card::setPower(unsigned short inputPower){
     power = inputPower;
 }
 
-void Card::setPriority(short inputPriority){
-    priority = inputPriority;
-}
-
 CardType Card::getType()const{
     return type;
 }
 
 unsigned short Card::getPower()const{
     return power;
-}
-
-
-short Card::getPriority()const{
-    return priority;
 }
