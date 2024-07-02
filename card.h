f9536f1ad7f60babd540949762d81c4344f8a0a9
ShahabSamariShams <shahabshams242@gmail.com>
2024-07-03 01:21:47 +0330

    Update card.h
    Removed the data members and member functions that did not belong to the card class.

--- card.h
+++ card.h
@@ -5,8 +5,6 @@
 
 #include "game.h"
 
-class Game;
-
 enum CardType{
     soldier,
     scarecrow, //Immediate effect.
@@ -26,19 +24,15 @@ class Card{
     //Setters:
         void setType(CardType);
         void setPower(unsigned short);
-        void setPriority(short);
     //Getters:
         CardType getType()const;
         unsigned short getPower()const;
-        short getPriority()const;
     //Functions to be overrided:
-        virtual void ability(Game&) = 0;
         virtual std::string getTypeName()const = 0;
         
     private:
         CardType type;
         unsigned short power;
-        short priority;
 };
 
 #endif
