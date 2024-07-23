0aa78c4c38937a1a226acab5b6a8ec6577c17768
ShahabSamariShams <shahabshams242@gmail.com>
2024-07-23 21:15:30 +0330

    Index of player in turn.

--- midGameData.h
+++ midGameData.h
@@ -7,6 +7,7 @@ struct MidGameData{
     //Indices:
         int indexOfWarStarter;
         int indexOfPeaceMarkOwner;
+        int indexOfPlayerInTurn;
     //Vectors that overlap with player list:
         std::vector <int> spyCount;
         std::vector <bool> passed;
